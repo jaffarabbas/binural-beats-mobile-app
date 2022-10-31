4a6b2af8cc154f8f3aaa9a54ce1a7bd8e8e599bd
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-31 19:53:56 +0500

    Updated lib/pages/login-screen/loginPage.dart at 31/10/2022 19:53:56

--- binural_app/lib/pages/login-screen/loginPage.dart
+++ binural_app/lib/pages/login-screen/loginPage.dart
@@ -19,20 +19,24 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(logo),
+            Container(
+              height: 120.h,
+              width: 120.h,
+              child: Image.asset(logo,fit: BoxFit.contain,),
+            ),
             SizedBox(
-              height: 15.h,
+              height: 20.h,
             ),
             Text(
               'Come Join Our Universe',
               style: TextStyle(fontSize: 25.sp, color: secondaryColor),
             ),
             SizedBox(
-              height: 15.h,
+              height: 20.h,
             ),
             LoginBtn(img: Image.asset(googleIcon), text: 'Login With Google'),
             SizedBox(
-              height: 15.h,
+              height: 20.h,
             ),
             LoginBtn(
                 img: Image.asset(facebookIcon), text: 'Login With Facebook')
