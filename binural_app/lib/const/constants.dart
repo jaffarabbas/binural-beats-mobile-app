976b01abe382012da53cbfcc761787c6be325e37
jaffarabbas <gamaportal8@gmail.com>
2022-11-05 10:40:26 +0500

    Updated lib/const/constants.dart at 05/11/2022 10:40:26

--- binural_app/lib/const/constants.dart
+++ binural_app/lib/const/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/animation.dart';
 double height = 667;
 double weight = 375;
 Size size = Size(weight, height);
+List<String> registerOptions = ['Sounds For Sleep','Sounds For Study','Sounds For Relaxation','Sounds For Theraphy'];
