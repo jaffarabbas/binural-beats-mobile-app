2557b5c233cac2539c3b37f38252c3e58cd35375
jaffarabbas <gamaportal8@gmail.com>
2022-11-18 23:48:57 +0500

    Updated lib/theme/colors.dart at 18/11/2022 23:48:57

--- binural_app/lib/theme/colors.dart
+++ binural_app/lib/theme/colors.dart
@@ -3,11 +3,24 @@ import 'package:flutter/animation.dart';
 const Color linearColor1 = Color(0xff8E2DE2);
 const Color linearColor2 = Color(0xff4A00E0);
 
-const List<Color> linearBackgroundColor = [
-  linearColor1,
-  linearColor2
-];
+const List<Color> linearBackgroundColor = [linearColor1, linearColor2];
 
 const Color primaryColor = Color(0xff4A00E0);
 const Color secondaryColor = Color(0xffFFFFFF);
 
+//cars
+
+List cardList = [
+  [
+    const Color(0xff6A3093),
+    const Color(0xffA044FF),
+  ],
+  [
+    const Color(0xff6A3093),
+    const Color(0xff2A5298),
+  ],
+  [
+    const Color(0xff1E3C72),
+    const Color(0xffA044FF),
+  ]
+];
