5b680193867ab5112ea76e21afa6200c40a9e97b
jaffarabbas <gamaportal8@gmail.com>
2022-11-18 23:48:57 +0500

    Updated lib/const/assets.dart at 18/11/2022 23:48:57

--- binural_app/lib/const/assets.dart
+++ binural_app/lib/const/assets.dart
@@ -1,3 +1,10 @@
 const String logo = "assets/logo/l1.png";
 const String googleIcon = "assets/images/g.png";
 const String facebookIcon = "assets/images/f.png";
+const String primary_drawer_logo = "assets/appbar/appbarLogo.svg";
+
+
+const String s1 = "assets/images/slider/s1.png";
+const String s2 = "assets/images/slider/s2.png";
+const String s3 = "assets/images/slider/s3.png";
+
