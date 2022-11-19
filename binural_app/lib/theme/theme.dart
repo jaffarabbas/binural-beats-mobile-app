c44ddcf715398e25e0a7af2dd5c8bf2ad915ffd8
jaffarabbas <gamaportal8@gmail.com>
2022-11-19 16:52:16 +0500

    Updated lib/theme/theme.dart at 19/11/2022 16:52:16

--- binural_app/lib/theme/theme.dart
+++ binural_app/lib/theme/theme.dart
@@ -16,6 +16,37 @@ const linearBackground = BoxDecoration(
   ),
 );
 
+const linearBackgroundBottomNavigation = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment(0.0, 7.0),
+    end: Alignment(0.0, 13.0), // 10% of the width, so there are ten blinds.
+    colors: linearBackgroundColor, // red to yellow
+    tileMode: TileMode.clamp, // repeats the gradient over the canvas
+  ),
+);
+
+//library
+const categoriesListDecoration = BoxDecoration(
+    color: linearColor1, borderRadius: BorderRadius.all(Radius.circular(10)));
+
+const categoriesSearchFeildContainerDecoration = BoxDecoration(
+    color: linearColor1, borderRadius: BorderRadius.all(Radius.circular(15)));
+
+const categoriesTextFeildDecoration = InputDecoration(
+  hintText: "Search",
+  fillColor: linearColor1,
+  contentPadding: EdgeInsets.only(top: 15),
+  hintStyle: TextStyle(
+    color: secondaryColor,
+  ),
+  prefixIcon: Icon(Icons.search,color: secondaryColor,size: 23,),
+  suffixIcon: Icon(Icons.mic,color: secondaryColor,size: 23,),
+  focusedBorder: InputBorder.none,
+  enabledBorder: InputBorder.none,
+  errorBorder: InputBorder.none,
+  disabledBorder: InputBorder.none,
+);
+//auth
 const loginBtnDecoration = BoxDecoration(
   borderRadius: BorderRadius.all(Radius.circular(10)),
   color: secondaryColor,
