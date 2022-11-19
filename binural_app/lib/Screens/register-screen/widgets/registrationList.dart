2f65650553716ba29f33cf16cde0d7be98f94159
jaffarabbas <gamaportal8@gmail.com>
2022-11-19 16:55:59 +0500

    Inserted lib/Screens/register-screen/widgets/registrationList.dart at 19/11/2022 16:55:59

---
+++ binural_app/lib/Screens/register-screen/widgets/registrationList.dart
@@ -0,0 +1,23 @@
+import 'package:binural_app/const/constants.dart';
+import 'package:binural_app/pages/register-screen/widgets/registerButton.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RegisterationList extends StatelessWidget {
+  const RegisterationList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.w),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: registerOptions.length,
+        itemBuilder: (context, index) {
+        return RegisterButton(name: registerOptions[index]);
+      },),
+    );
+  }
+}
