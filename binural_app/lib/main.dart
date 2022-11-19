2c68cdadcb33af0853ee0570d45e817f121d971a
jaffarabbas <gamaportal8@gmail.com>
2022-11-19 16:52:16 +0500

    Updated lib/main.dart at 19/11/2022 16:52:16

--- binural_app/lib/main.dart
+++ binural_app/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:binural_app/const/constants.dart';
 import 'package:binural_app/pages/dashboard-screen/dashboard-screen.dart';
+import 'package:binural_app/pages/home_screen/home_screen.dart';
 import 'package:binural_app/pages/login-screen/loginPage.dart';
 import 'package:binural_app/pages/register-screen/registerScreen.dart';
+import 'package:binural_app/providers/changeBottomNavigationProvider.dart';
 import 'package:binural_app/theme/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,17 +17,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: size,
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: maintheme,
-          home: const DashBoardScreen(),
-        );
-      },
+    return MultiProvider(
+      providers: [
+          ChangeNotifierProvider(create: (_) => ChnageBottomNavgationProvider()),
+      ],
+      child: Builder(
+        builder: (context) {
+          return ScreenUtilInit(
+            designSize: size,
+            minTextAdapt: true,
+            splitScreenMode: true,
+            builder: (context, child) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: maintheme,
+                home: const HomeScreen(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
