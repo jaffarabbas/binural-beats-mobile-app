aed604405e6b75b7faae88cc2ebafaf572916148
jaffarabbas <gamaportal8@gmail.com>
2022-11-19 16:55:59 +0500

    Inserted lib/Screens/dashboard-screen/dashboard-screen.dart at 19/11/2022 16:55:59

---
+++ binural_app/lib/Screens/dashboard-screen/dashboard-screen.dart
@@ -0,0 +1,45 @@
+import 'package:binural_app/components/appBar.dart';
+import 'package:binural_app/components/appBottomNavigationBar.dart';
+import 'package:binural_app/components/appDrawer.dart';
+import 'package:binural_app/components/mainCard.dart';
+import 'package:binural_app/components/mainSoundSlider.dart';
+import 'package:binural_app/const/assets.dart';
+import 'package:binural_app/const/constants.dart';
+import 'package:binural_app/Screens/dashboard-screen/widgets/header.dart';
+import 'package:binural_app/Screens/dashboard-screen/widgets/headerSlider.dart';
+import 'package:binural_app/components/soundSection.dart';
+import 'package:binural_app/theme/colors.dart';
+import 'package:binural_app/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/changeBottomNavigationProvider.dart';
+
+class DashBoardScreen extends StatelessWidget {
+  const DashBoardScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: linearBackground,
+        child: Container(
+          margin: EdgeInsets.only(top: 80.h),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              DashboardHeader(),
+              SoundSectionList()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
