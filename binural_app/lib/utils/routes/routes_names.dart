baba82c21648a183c6feb4a213e55dcb6c2a5ef2
jaffarabbas <gamaportal8@gmail.com>
2022-11-20 10:32:30 +0500

    Updated lib/utils/routes/routes_names.dart at 20/11/2022 10:32:30

--- binural_app/lib/utils/routes/routes_names.dart
+++ binural_app/lib/utils/routes/routes_names.dart
@@ -6,5 +6,5 @@ class RoutesName{
   static const String library = 'library_view';
   static const String sound_details = 'sound_details_view';
   static const String splash = 'splash_view';
-  static const String profle = 'profle_view';
+  static const String profile = 'profile_view';
 }
