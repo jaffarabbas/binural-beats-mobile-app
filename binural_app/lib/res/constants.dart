18b7098a139ce6e0a86d0ccc94e125e3e32deeee
jaffarabbas <gamaportal8@gmail.com>
2022-11-20 10:32:30 +0500

    Updated lib/res/constants.dart at 20/11/2022 10:32:30

--- binural_app/lib/res/constants.dart
+++ binural_app/lib/res/constants.dart
@@ -5,9 +5,7 @@ import 'package:binural_app/view/login_view/login_view.dart';
 import 'package:binural_app/view/notfound_view/notfound_view.dart';
 import 'package:binural_app/view/profile_view/profile_view.dart';
 import 'package:binural_app/view/register_view/register_view.dart';
-import 'package:binural_app/view/sound_details_screen/sound_detials_screen.dart';
-import 'package:binural_app/view/splash-screen/splash-view.dart';
-import 'package:flutter/animation.dart';
+import 'package:binural_app/view/sound_details_screen/sound_detials_view.dart';
 import 'package:flutter/material.dart';
 
 import 'assets.dart';
@@ -23,19 +21,20 @@ List<String> registerOptions = [
 ];
 List<String> categories = ['Sleep', 'Study', 'Relaxation', 'Theraphy'];
 List<String> sliderImages = [s1, s2, s3];
-List widgetsForRoute = const [
-  {"route_name": "home", "route": HomeView()},
-  {"route_name": "login", "route": LoginView()},
-  {"route_name": "register", "route": RegisterView()},
-  {"route_name": "dashboard", "route": DashBoardView()},
-  {"route_name": "library", "route": LibraryView()},
-  {"route_name": "splash", "route": SplashView()},
-  {"route_name": "sound_details", "route": SoundDetailsView()},
-  {"route_name": "profile", "route": ProfileView()},
-  {"route_name": "notfound", "route": NotFounView}
-];
+// List widgetsForRoute = const [
+//   {"route_name": "home", "route": HomeView()},
+//   {"route_name": "login", "route": LoginView()},
+//   {"route_name": "register", "route": RegisterView()},
+//   {"route_name": "dashboard", "route": DashBoardView()},
+//   {"route_name": "library", "route": LibraryView()},
+//   {"route_name": "splash", "route": SplashView()},
+//   {"route_name": "sound_details", "route": SoundDetailsView()},
+//   {"route_name": "profile", "route": ProfileView()},
+//   {"route_name": "notfound", "route": NotFounView}
+// ];
 List<Widget> widgitsList = const [
   LibraryView(),
   DashBoardView(),
-  ProfileView()
+  ProfileView(),
+  SoundDetailsView()
 ];
