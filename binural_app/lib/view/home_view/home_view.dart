1ba672cfb5a03d5c7bea526f0037fd15cd7bc80e
jaffarabbas <gamaportal8@gmail.com>
2022-11-20 10:32:30 +0500

    Updated lib/view/home_view/home_view.dart at 20/11/2022 10:32:30

--- binural_app/lib/view/home_view/home_view.dart
+++ binural_app/lib/view/home_view/home_view.dart
@@ -5,7 +5,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
 
-import '../../res/components/appBar.dart';                                
+import '../../res/components/appBar.dart';
 import '../../res/components/appBottomNavigationBar.dart';
 import '../../res/components/appDrawer.dart';
 import '../../providers/changeBottomNavigationProvider.dart';
@@ -13,13 +13,15 @@ import '../../providers/changeBottomNavigationProvider.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-                                                                                                                                                        @override
-                                                                                                                                                        Widget build(BuildContext context) {
-                                                                                                                                                          final changeBottomNavigationIndex =
-                                                                                                                                                              Provider.of<ChnageBottomNavgationProvider>(context, listen: true);
+  @override
+  Widget build(BuildContext context) {
+    final changeBottomNavigationIndex =
+        Provider.of<ChangeBottomNavgationProvider>(context, listen: true);
 
-                                                                                                                                                          return Scaffold(
-      appBar: CustomAppBar(isContainAction: false,),
+    return Scaffold(
+      appBar: CustomAppBar(
+        isContainAction: false,
+      ),
       extendBodyBehindAppBar: true,
       body: Container(
         height: double.infinity,
