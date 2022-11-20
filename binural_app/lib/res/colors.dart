d5abfe89bfc36759ff913a46b7776f90eb88df0e
jaffarabbas <gamaportal8@gmail.com>
2022-11-20 10:32:30 +0500

    Updated lib/res/colors.dart at 20/11/2022 10:32:30

--- binural_app/lib/res/colors.dart
+++ binural_app/lib/res/colors.dart
@@ -3,7 +3,11 @@ import 'package:flutter/animation.dart';
 const Color linearColor1 = Color(0xff8E2DE2);
 const Color linearColor2 = Color(0xff4A00E0);
 
+const Color linearColor3 = Color(0xff6A3093);
+const Color linearColor4 = Color(0xffA044FF);
+
 const List<Color> linearBackgroundColor = [linearColor1, linearColor2];
+const List<Color> linearBackgroundColorSoundDetails = [linearColor3, linearColor4];
 
 const Color primaryColor = Color(0xff4A00E0);
 const Color secondaryColor = Color(0xffFFFFFF);
