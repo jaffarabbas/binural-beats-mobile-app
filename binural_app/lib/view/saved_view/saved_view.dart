87faad2ebdfc826e8a18f7690ea784cab9aa5504
jaffarabbas <gamaportal8@gmail.com>
2022-11-22 20:47:18 +0500

    Inserted lib/view/saved_view/saved_view.dart at 22/11/2022 20:47:18

---
+++ binural_app/lib/view/saved_view/saved_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SavedView extends StatelessWidget {
+  const SavedView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
