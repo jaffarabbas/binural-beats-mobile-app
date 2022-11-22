fd75aa2d355a3e38bdf46f7fb29b5190c66e14a1
jaffarabbas <gamaportal8@gmail.com>
2022-11-22 20:47:18 +0500

    Updated lib/utils/routes/routes.dart at 22/11/2022 20:47:18

--- binural_app/lib/utils/routes/routes.dart
+++ binural_app/lib/utils/routes/routes.dart
@@ -1,8 +1,9 @@
-
 import 'package:binural_app/res/constants.dart';
+import 'package:binural_app/view/favourite_view/favourite_view.dart';
 import 'package:binural_app/view/home_view/home_view.dart';
 import 'package:binural_app/view/login_view/login_view.dart';
 import 'package:binural_app/view/notfound_view/notfound_view.dart';
+import 'package:binural_app/view/saved_view/saved_view.dart';
 import 'package:binural_app/view/sound_details_screen/sound_detials_view.dart';
 import 'package:binural_app/view/sound_player_view/sound_player_view.dart';
 import 'package:binural_app/view/splash_view/splash_view.dart';
@@ -12,12 +13,11 @@ import 'package:flutter/widgets.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings routeSettings) {
-    final arguments = routeSettings.arguments; 
+    final arguments = routeSettings.arguments;
     // return MaterialPageRoute(builder: (BuildContext context) => widgetsForRoute[RoutesName.dashboard]);
     switch (routeSettings.name) {
       case RoutesName.home:
-        return MaterialPageRoute(
-            builder: (BuildContext context) => HomeView());
+        return MaterialPageRoute(builder: (BuildContext context) => HomeView());
       case RoutesName.login:
         return MaterialPageRoute(
             builder: (BuildContext context) => LoginView());
@@ -26,13 +26,22 @@ class Routes {
             builder: (BuildContext context) => SplashView());
       case RoutesName.sound_details:
         return MaterialPageRoute(
-              builder: (BuildContext context) => SoundDetailsView());
+            builder: (BuildContext context) => SoundDetailsView());
       case RoutesName.sound_player:
         return MaterialPageRoute(
-              builder: (BuildContext context) => SoundPlayerView());
+            builder: (BuildContext context) => SoundPlayerView());
+      case RoutesName.favourite:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => FavouriteView());
+      case RoutesName.saved:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => SavedView());
       default:
         return MaterialPageRoute(
-            builder: (BuildContext context) => NotFounView(page: routeSettings.name.toString(),));
+          builder: (BuildContext context) => NotFounView(
+            page: routeSettings.name.toString(),
+          ),
+        );
     }
   }
 }
