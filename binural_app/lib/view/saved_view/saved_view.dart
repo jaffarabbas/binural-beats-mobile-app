082d7dfd45de023cbf67d69b1c6715cd7e411b08
jaffarabbas <gamaportal8@gmail.com>
2022-11-23 18:36:07 +0500

    Updated lib/view/saved_view/saved_view.dart at 23/11/2022 18:36:07

--- binural_app/lib/view/saved_view/saved_view.dart
+++ binural_app/lib/view/saved_view/saved_view.dart
@@ -1,11 +1,100 @@
+import 'package:binural_app/res/colors.dart';
+import 'package:binural_app/res/components/appBar.dart';
+import 'package:binural_app/res/components/appDrawer.dart';
+import 'package:binural_app/res/components/mainCard.dart';
+import 'package:binural_app/res/components/secondaryCard.dart';
+import 'package:binural_app/res/theme/theme.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SavedView extends StatelessWidget {
   const SavedView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: CustomAppBar(isContainAction: false),
+      body: Container(
+        decoration: linearBackground,
+        child: Container(
+          margin: EdgeInsets.only(top: 70.h),
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.only(bottom: 20.h),
+                child: Text(
+                  'Saved',
+                  style: TextStyle(
+                      fontSize: 30.sp,
+                      fontWeight: FontWeight.bold,
+                      color: secondaryColor),
+                ),
+              ),
+              Container(
+                height: 500.h,
+                padding: EdgeInsets.zero,
+                child: ListView.builder(
+                   padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  itemCount: 50,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(top: 10.h),
+                      padding: EdgeInsets.symmetric(vertical: 10.h),
+                      decoration: sampleCardDecoration,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SecondaryCard(
+                              color: cardList[0],
+                              frequencfyInHurdz: [],
+                              spots: []),
+                          Container(
+                            // padding: EdgeInsets.only(left: 5.sp),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Sleep Binural',
+                                  style: TextStyle(
+                                      fontSize: 16.sp, color: secondaryColor),
+                                ),
+                                Text(
+                                  'Gama 430 Hz',
+                                  style: TextStyle(
+                                      fontSize: 13.sp, color: secondaryColor),
+                                ),
+                              ],
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              child: Icon(
+                                Icons.play_circle_fill,
+                                size: 50.sp,
+                                color: secondaryColor,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      drawer: AppDrawer(),
+    );
   }
 }
