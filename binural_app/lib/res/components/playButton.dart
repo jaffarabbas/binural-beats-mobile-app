51f0aed5e0dfa02e742e621ed83f33701c631820
jaffarabbas <gamaportal8@gmail.com>
2022-11-30 22:29:00 +0500

    Updated lib/res/components/playButton.dart at 30/11/2022 22:29:00

--- binural_app/lib/res/components/playButton.dart
+++ binural_app/lib/res/components/playButton.dart
@@ -1,5 +1,6 @@
 import 'package:binural_app/res/colors.dart';
 import 'package:binural_app/res/theme/theme.dart';
+import 'package:binural_app/utils/routes/routes_names.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,7 +14,9 @@ class PlayButton extends StatelessWidget {
     return Container(
       decoration: soundPlayButtonDecoration,
       child: IconButton(
-        onPressed: () {},
+        onPressed: () {
+        Navigator.pushNamed(context, RoutesName.sound_player);
+        },
         iconSize: 40.sp,
         icon: const Icon(
           Icons.play_arrow,
